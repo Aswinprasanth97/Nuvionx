dc432168769af0d2b6c4d4c1334d55cd805fbfe3
Aswin Prsaanth <aswinprasanth97@gmail.com>
2025-05-22 01:19:08 +0530

    bg-background updated

---
+++ main.js
@@ -0,0 +1,6 @@
+document.getElementById('scroll-to-top').addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth' // Optional: for smooth scrolling
+    });
+});
