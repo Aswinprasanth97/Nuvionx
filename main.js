7eb378ef76692884b1b1999c73e3ead51747633f
Aswin Prsaanth <aswinprasanth97@gmail.com>
2025-05-24 10:33:10 +0530

    responsive - about us page

--- main.js
+++ main.js
@@ -4,3 +4,21 @@ document.getElementById('scroll-to-top').addEventListener('click', () => {
         behavior: 'smooth' // Optional: for smooth scrolling
     });
 });
+document.getElementById('nav-main').addEventListener('click', () => {
+    console.log("clicked");
+
+    let dropmenu = document.getElementById('nav-drop');
+    if (dropmenu.classList.contains('hidden')) { dropmenu.classList.remove('hidden'); }
+    else { dropmenu.classList.add('hidden'); }
+
+})
+if (!document.getElementById('nav-drop').classList.contains('hidden')) {
+    document.documentElement.addEventListener('click', () => {
+        console.log('clicked');
+
+        let dropmenu = document.getElementById('nav-drop');
+        if (!dropmenu.classList.contains('hidden')) {
+            dropmenu.classList.add('hidden');
+        }
+    });
+}
